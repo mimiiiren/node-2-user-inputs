af1e1527441744dba34167ebd0496fe1d6ddef35
mimiiiren <mimiren015@gmail.com>
2026-02-26 16:47:54 -0500

    solve dog age calculator

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -8,3 +8,18 @@ Run this file with the node command:
 node dogAgeCalculator.js
 
 */
+let dogName = process.argv[2]
+let humanAge = Number(process.argv[3])
+let dogAge;
+
+    if (humanAge === 1) {
+        dogAge = 15
+    } else if (humanAge === 2) {
+        dogAge = 15 + 9
+    } else if (humanAge > 2) {
+        dogAge = 24 + 5 * (humanAge - 2)
+    }
+
+
+console.log(`Your dog, ${dogName}, is ${dogAge} years old,
+but that's ${humanAge} years old in dog years!`);
