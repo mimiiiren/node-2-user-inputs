ac861f28632fd7a22528ec4d04eef22005cee451
mimiiiren <mimiren015@gmail.com>
2026-03-04 12:32:17 -0500

    code comments

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -8,10 +8,14 @@ Run this file with the node command:
 node dogAgeCalculator.js
 
 */
+// user input, declare variables as arguments in cli
 let dogName = process.argv[2]
 let humanAge = Number(process.argv[3])
+
+// js output depending on user input
 let dogAge;
 
+// if condition to check dogAge
     if (humanAge === 1) {
         dogAge = 15
     } else if (humanAge === 2) {
